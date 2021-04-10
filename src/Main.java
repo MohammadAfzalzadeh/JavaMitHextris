cfe200034511d9304f1a3a7248720b29b0d17608
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2021-04-10 17:23:01 +0430

    Add Main Class and first UI and add blook and move it

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Pane root = new Pane();
+        Scene scene = new Scene(root,100,100,Color.BLACK);
+        //setBackgroundImage(root);
+        Blook blook = new Blook(root);
+        blook.move();
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(600);
+        primaryStage.setWidth(800);
+       // primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
